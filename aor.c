e5605836477d6223d0aed3ebad98c6a18291f444
Arthur O. Reis <aor@cesar.school>
2018-11-21 20:21:34 -0300

    accepted

--- aor.c
+++ aor.c
@@ -2,25 +2,25 @@
 #include <stdlib.h>
 #include <string.h>
 
-// void sort(char const *array[], int size);
-// void swap(char const **a, char const **b);
+void quicksort(char array[][20], int low, int high);
+int partition(char array[][20], int low, int high);
 void printArray(char array[][20], int sizeArray);
 
 int main(void) {
 
-	int N;
-	scanf("%d", &N);
+	int size;
+	scanf("%d", &size);
 	/* condicional */
-	if (N > 100 || N < 0) {
+	if (size > 100 || size < 0) {
 		return 0;
 	}
 
 	int sc = 0; /* se comportou */
 	int nsc = 0; /* nao se comportou */
-	char myArray[N][20];
+	char myArray[size][20];
 	char simbol;
 
-	for (int i = 0; i < N; i++) {
+	for (int i = 0; i < size; i++) {
 		scanf(" %c %s", &simbol, myArray[i]);
 		if (simbol == '+') {
 			sc++;
@@ -29,19 +29,42 @@ int main(void) {
 		}
 	}
 
-	printArray(myArray, N);
-	printf("sc = %d ; nsc = %d\n", sc, nsc);
+	quicksort(myArray, 0, size-1);
+	printArray(myArray, size);
+	printf("Se comportaram: %d | Nao se comportaram: %d\n", sc, nsc);
 
 	return 0;
 }
 
-// void sort(char const *array[], int size) {
-// 	int i;
-// 	int pivot = 0;
-// 	if (size <= 1) {
-// 		return;
-// 	}
-// }
+void quicksort(char array[][20], int low, int high) {
+	if (low < high) {
+		int pIndex = partition(array, low, high);
+		quicksort(array, low, pIndex-1);
+		quicksort(array, pIndex+1, high);
+	}
+}
+
+int partition(char array[][20], int low, int high) {
+	char pivot[20];
+	strcpy(pivot, array[high]);
+	char i = low-1;
+	for (int j = low; j <= high-1; j++) {
+		if (strcmp(array[j], pivot) < 0) {
+			i++;
+			/* swap */
+			char temp[20];
+			strcpy(temp, array[i]);
+			strcpy(array[i], array[j]);
+			strcpy(array[j], temp);
+		}
+	}
+	/* swap */
+	char temp[20];
+	strcpy(temp, array[i+1]);
+	strcpy(array[i+1], array[high]);
+	strcpy(array[high], temp);
+	return (i+1);
+}
 
 void printArray(char array[][20], int sizeArray) {
 	for (int i = 0; i < sizeArray; i++) {
